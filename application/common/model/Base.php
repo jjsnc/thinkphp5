543211dd988eb7dbdc3471637262d4aa396385c1
zhuowenxuan <zhuowenxuan@youfish.com>
2022-12-17 17:48:27 +0800

    init

---
+++ application/common/model/Base.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: baidu
+ * Date: 17/7/27
+ * Time: 下午5:57
+ */
+
+namespace app\common\model;
+
+use think\Model;
+
+class Base extends Model
+{
+
+  // 插入库的时间
+  protected  $autoWriteTimestamp = true;
+
+  /**
+   * 新增
+   * @param $data
+   * @return mixed
+   */
+  public function add($data)
+  {
+
+    if (!is_array($data)) {
+      exception('传递数据不合法');
+    }
+
+    $this->allowField(true)->save($data);
+
+    return $this->id;
+  }
+}
