4573da2b296cb387d6ddb72d5c5b877aedf78298
zhuowenxuan <zhuowenxuan@youfish.com>
2022-12-18 14:38:34 +0800

    添加新闻

--- application/extra/qiniu.php
+++ application/extra/qiniu.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: baidu
@@ -6,8 +7,8 @@
  * Time: 上午12:37
  */
 return [
-    'ak' => '8VmI78kVmGn9VahZi-KjIokyubxWxteHVkA2pbjd',
-    'sk' => 'S64N4SPi2atMSVVL9uBoXzG4DJtDCQVoOtou0VIu',
-    'bucket' => 'imoocapp',
-    'image_url' => 'http://otwueljs0.bkt.clouddn.com',
+  'ak' => '8VmI78kVmGn9VahZi-KjIokyubxWxteHVkA2pbjd',
+  'sk' => 'S64N4SPi2atMSVVL9uBoXzG4DJtDCQVoOtou0VIu',
+  'bucket' => 'imoocapp',
+  'image_url' => 'https://img2.baidu.com/it/u=1395980100,2999837177&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=675',
 ];
